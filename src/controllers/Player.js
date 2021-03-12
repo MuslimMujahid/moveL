5313ea23996ddc210955248a2512e05fcbc46d5d
MuslimMujahid <muslimmujahid1712@gmail.com>
2021-03-12 16:47:08 +0700

    create libraries api

--- src/controllers/Player.js
+++ src/controllers/Player.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 const time = require('../utils/time');
 
 exports.play = function (req, res) {
-    exec(`vlc ${req.body.URL}`, async (error, stdout, stderr) => {
+    exec(`vlc ${encodeURI(req.body.URL)}`, async (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
@@ -14,6 +14,6 @@ exports.play = function (req, res) {
             console.log('you stopped at', time.getTimeMiliseconds()-start);
         }
     });
-    res.status(200).send('berhasil');
+    res.status(200).send('Playing');
 }
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const config = require('config');
 const PlayerRouter = require('./routers/Player');
+const LibrariesRouter = require('./routers/Libraries');
 
 const app = express();
 
@@ -7,10 +9,15 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use('/public', express.static('public'));
+for (const lib of config.get('Libraries')) {
+    app.use('/libraries', express.static(lib));
+}
+
 app.get('/', (req, res) => {
     res.send('berhasil');
 })
 app.use('/player', PlayerRouter);
+app.use('/libraries', LibrariesRouter);
 
 let port = process.env.PORT || 5000;
 app.listen(port, (err) => {
